7ed6143652f59e5a5d98bf44301326c08be5d380
David Wasilewski <wasile19@msu.edu>
2024-02-15 14:10:05 -0500

    added data page

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,27 @@
-//100m, long jump, shot put, high jump, 400m, 110m hurdles, discus, pole vault, javelin, 1500m
-const trackEvents = [
+//Keep jumps in cm fo rpoint calculations
+var trackEvents = [
     {title: '100m', A: 25.4347, B: 18, C: 1.81, isTimed: true, day1: true, highScore: 10.6, points: 952, worldRecord: 10.55, WRpoints: 963, step: 0.1},
-    {title: 'Long Jump', A: 0.14354, B: 220, C: 1.4, isTimed: false, day1: true, highScore: 7.06, points: 788, worldRecord: 7.80, WRpoints: 1010, step: 0.2},
+    {title: 'Long Jump', A: 0.14354, B: 220, C: 1.4, isTimed: false, day1: true, highScore: 706, points: 788, worldRecord: 7.80, WRpoints: 1010, step: 0.2},
     {title: 'Shot Put', A: 51.39, B: 1.5, C: 1.05, isTimed: false, day1: true, highScore: 15.14, points: 798, worldRecord: 16.00, WRpoints: 851, step: 0.5},
-    {title: 'High Jump', A: 0.8465, B: 75, C: 1.42, isTimed: false, day1: true, highScore: 1.91, points: 723, worldRecord: 2.05, WRpoints: 850, step: 0.03},
+    {title: 'High Jump', A: 0.8465, B: 75, C: 1.42, isTimed: false, day1: true, highScore: 191, points: 723, worldRecord: 2.05, WRpoints: 850, step: 0.03},
     {title: '400m', A: 1.53775, B: 82, C: 1.81, isTimed: true, day1: true, highScore: 47.52, points: 933, worldRecord: 48.42, WRpoints: 889, step: 0.5},
     {title: '110m Hurdles', A: 5.74352, B: 28.5, C: 1.92, isTimed: true, day1: false, highScore: 14.74, points: 885, worldRecord: 13.75, WRpoints: 1007, step: 0.1},
     {title: 'Discus', A: 12.91, B: 4, C: 1.1, isTimed: false, day1: false, highScore: 50.66, points: 884, worldRecord: 50.54, WRpoints: 882, step: 1},
     {title: 'Pole Vault', A: 0.2797, B: 100, C: 1.35, isTimed: false, day1: false, highScore: 5.20, points: 972, worldRecord: 5.45, WRpoints: 1051, step: 0.1},
     {title: 'Javelin', A: 10.14, B: 7, C: 1.08, isTimed: false, day1: false, highScore: 58.18, points: 710, worldRecord: 71.90, WRpoints: 918, step: 1},
     {title: '1500m', A: 0.03768, B: 480, C: 1.85, isTimed: true, day1: false, highScore: 280.73, points: 676, worldRecord: 276.11, WRpoints: 705, step: 1},
-];
+].map(event => {
+    if (event.title === '1500m') {
+        event.text = Math.floor(event.highScore / 60) + ":" + Math.floor(event.highScore % 60) + ":" + Math.floor((event.highScore % 1) * 100) + " Minutes";
+    } else if (event.isTimed) {
+        event.text = event.highScore.toFixed(2) + " Seconds";
+    } else {
+        if (event.title === "Long Jump" || event.title === "High Jump") event.text = (event.highScore / 100).toFixed(2) + " Meters";
+        else event.text = (event.highScore).toFixed(2) + " Meters";
+    }
+    return event;
+});
+
 
         var sessionStart = Date.now(); // Initialize sessionStart when the user loads the website
 
@@ -320,7 +331,9 @@ const trackEvents = [
               if (!startTimestamp) startTimestamp = timestamp;
               const progress = Math.min((timestamp - startTimestamp) / duration, 1);
               element.textContent = (progress * highScore).toFixed(2);
-              if (progress < 1) {
+              if (progress >= 1) {
+                element.textContent = trackEvents[element.getAttribute('data-index')].text;
+              } else {
                 window.requestAnimationFrame(step);
               }
             };
@@ -354,6 +367,7 @@ const trackEvents = [
               observer.observe(document.getElementById(`highScore-${index}`));
             });
           });
+          
 
         events.forEach(addEvent);
         timedEvents.forEach(addTrackEvent);
